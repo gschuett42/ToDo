1ee17e5089b92250b8592aacd371995aa5e7c1f8
schue <gschuett@htl-steyr.ac.at>
2020-11-27 12:20:31 +0100

    fdg

--- src/PriorityController.java
+++ src/PriorityController.java
@@ -8,6 +8,7 @@ import model.Status;
 public class PriorityController {
     public TextField nameTextField;
     public ListView<Priority> priorityListView;
+    public Priority selectedItem = null;
 
     public void initialize(){
         priorityListView.setItems(Priority.getList());
@@ -16,18 +17,31 @@ public class PriorityController {
         Priority p = priorityListView.getSelectionModel().getSelectedItem();
         if(p != null){
             nameTextField.setText(p.getName());
+            selectedItem = p;
         }
     }
 
     public void cancelClicked(ActionEvent actionEvent) {
+        //close dialog
     }
 
     public void safeClicked(ActionEvent actionEvent) {
+        if(selectedItem !=null){
+            //update
+        }else{
+            //insert
+        }
     }
 
     public void deleteClicked(ActionEvent actionEvent) {
+        if(selectedItem!=null){
+            //delete
+        }
     }
 
     public void newClicked(ActionEvent actionEvent) {
+        selectedItem = null;
+        nameTextField.clear();
+        priorityListView.getSelectionModel().clearSelection();
     }
 }
