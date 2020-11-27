cb4f75bd9d2d020d96b2813920227f849a909215
Alexa <amusat@htl-steyr.ac.at>
2020-11-27 12:31:07 +0100

    eeeeee

--- src/StatusController.java
+++ src/StatusController.java
@@ -7,25 +7,44 @@ import model.Status;
 public class StatusController {
     public TextField nameTextField;
     public ListView<Status> statusListView;
-    public void initialize(){
+    public Status selectedItem = null;
+
+
+    public void initialize() {
         statusListView.setItems(Status.getList());
     }
+
     public void itemSelected(MouseEvent mouseEvent) {
         Status s = statusListView.getSelectionModel().getSelectedItem();
-        if(s != null){
+
+        if (s != null) {
             nameTextField.setText(s.getName());
+            selectedItem = s;
         }
+
     }
 
     public void saveClicked(ActionEvent actionEvent) {
+        if (selectedItem != null) {
+            //update existing one
+        } else {
+// insert new
+        }
     }
 
     public void escapeClicked(ActionEvent actionEvent) {
+        //close dialog
     }
 
     public void newClicked(ActionEvent actionEvent) {
+        selectedItem = null;
+        nameTextField.clear();
+        statusListView.getSelectionModel().clearSelection();
     }
 
     public void deleteClicked(ActionEvent actionEvent) {
+        if (selectedItem != null) {
+            //delete item
+        }
     }
 }
