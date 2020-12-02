31cd25f0dcf7de2ea1faec953b95e1eda2d88fa8
schue <gschuett@htl-steyr.ac.at>
2020-12-02 09:09:02 +0100

    fdg

--- src/Controller.java
+++ src/Controller.java
@@ -2,11 +2,24 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import model.Priority;
+import model.Status;
+import model.ToDo;
 
 import java.io.IOException;
 
 public class Controller {
+    public ListView<ToDo> todoListView;
+    public ComboBox<Priority> priorityComboBox;
+    public ComboBox<Status> statusComboBox;
+    public TextField ticketnameTextField;
+    public Pane contentPane;
+
     public void onStatusClicked(ActionEvent actionEvent) {
         try {
             Parent root = FXMLLoader.load(getClass().getResource("status.fxml"));
